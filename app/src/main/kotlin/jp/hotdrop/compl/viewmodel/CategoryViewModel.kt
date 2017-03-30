bb9040fa3158a4f105c9bb9d7a45628661723f55
kenji <gunsecondstage0106@gmail.com>
2017-03-30 22:36:26 +0900

    CategoryFragmentのcardViewのbackgroundColorをテーマカラーにするのは変に鮮やかすぎたので左にcolorborderをつける程度にした。

--- app/src/main/kotlin/jp/hotdrop/compl/viewmodel/CategoryViewModel.kt
+++ app/src/main/kotlin/jp/hotdrop/compl/viewmodel/CategoryViewModel.kt
@@ -18,7 +18,7 @@ class CategoryViewModel(var category: Category, val context: Context) {
 
     @ColorRes
     fun getColorRes(): Int {
-        return ColorUtil.getResLight(category.colorType, context)
+        return ColorUtil.getResNormal(category.colorType, context)
     }
 
     fun change(vm: CategoryViewModel) {
