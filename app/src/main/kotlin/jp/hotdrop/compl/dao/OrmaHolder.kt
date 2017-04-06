190e4832a207b6bbc04d7460607b4b8f09a58087
kenji <gunsecondstage0106@gmail.com>
2017-04-07 00:10:38 +0900

    Revert OrmaHolder.kt ... I'm so tired

--- app/src/main/kotlin/jp/hotdrop/compl/dao/OrmaHolder.kt
+++ app/src/main/kotlin/jp/hotdrop/compl/dao/OrmaHolder.kt
@@ -11,16 +11,20 @@ object OrmaHolder {
     private val DB_NAME = "compl.orma.db"
     lateinit var buildDB: OrmaDatabase
 
-    fun initialize(context: Context) {
-        buildDB = OrmaDatabase.builder(context)
-                /*.migrationStep(3, object : ManualStepMigration.ChangeStep() {
-                    override fun change(helper: ManualStepMigration.Helper) {
-                        helper.renameColumn("Category", "order", "viewOrder")
-                        helper.renameColumn("Company", "order", "viewOrder")
-                    }
-                })*/
-                .writeOnMainThread(AccessThreadConstraint.NONE)
-                .name(DB_NAME)
-                .build()
+    fun initialize(context: Context, productMode: Boolean = true) {
+        if(productMode) {
+            buildDB = OrmaDatabase.builder(context)
+                    /*.migrationStep(3, object : ManualStepMigration.ChangeStep() {
+                        override fun change(helper: ManualStepMigration.Helper) {
+                            helper.renameColumn("Category", "order", "viewOrder")
+                            helper.renameColumn("Company", "order", "viewOrder")
+                        }
+                    })*/
+                    .writeOnMainThread(AccessThreadConstraint.NONE)
+                    .name(DB_NAME)
+                    .build()
+        } else {
+            buildDB = OrmaDatabase.builder(context).name(null).build()
+        }
     }
 }
