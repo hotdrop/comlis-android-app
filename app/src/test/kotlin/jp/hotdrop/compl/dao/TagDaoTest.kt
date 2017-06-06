1f79b163cdf4347a5e3b32e4a1d27e4d614e0e29
kenji <gunsecondstage0106@gmail.com>
2017-06-06 22:50:57 +0900

    ずっと追加していなかったTagDaoのテストコードを追加

--- app/src/test/kotlin/jp/hotdrop/compl/dao/TagDaoTest.kt
+++ app/src/test/kotlin/jp/hotdrop/compl/dao/TagDaoTest.kt
@@ -2,9 +2,14 @@ package jp.hotdrop.compl.dao
 
 import android.content.Context
 import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
 import jp.hotdrop.compl.model.OrmaDatabase
+import jp.hotdrop.compl.model.Tag
 import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class TagDaoTest {
 
     private lateinit var tagDao: TagDao
@@ -18,5 +23,59 @@ class TagDaoTest {
         val orma = OrmaDatabase.builder(getContext()).name(null).build()
         val ormaHolder = OrmaHolder(orma)
         tagDao = TagDao(ormaHolder)
+    }
+
+    @Test
+    fun insertAndFindTest() {
+        val tagName = "test1"
+        val tag = createTag(tagName)
+        tagDao.insert(tag)
+
+        val tagFromDb = tagDao.find(tagName)
+        assertCompareTag(tag, tagFromDb)
+    }
+
+    @Test
+    fun findAllAndInIdTest() {
+        val tagNames = mutableListOf("test1", "test2", "test3", "test4", "test5")
+        tagNames.forEach { tagDao.insert(createTag(it)) }
+        val tags = tagDao.findAll().blockingGet().toList()
+        val ids = tags.filter{ tag -> tag.id%2 == 0 }.map { it.id }
+        val tagByEvenNumberId = tagDao.findInId(ids)
+        assert(ids.size == 3)
+        tagByEvenNumberId.forEach { tag ->
+            assert(tagNames.contains(tag.name))
+        }
+    }
+
+    fun countByAttachCompanyTest() {
+    }
+
+    fun updateTest() {
+    }
+
+    fun updateAllOrderTest() {
+
+    }
+
+    fun deleteTest() {
+    }
+
+    fun existTest() {
+
+    }
+
+    fun existExclusionIdTest() {
+    }
+
+    private fun createTag(argName: String) = Tag().apply {
+        name = argName
+        colorType = "ブルー"
+    }
+
+    private fun assertCompareTag(t1: Tag, t2: Tag) {
+        assert(t1.name == t2.name)
+        assert(t1.colorType == t2.colorType)
+        assert(t1.registerDate == t2.registerDate)
     }
 }
